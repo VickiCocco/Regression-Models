375dbde4ed405b3b8848e6ffd3109896c68088fb
VickiCocco <vickicocco@gmail.com>
2017-06-12 08:36:01 -0400

    initial

---
+++ installlibraries.R
@@ -0,0 +1,7 @@
+installlibraries <-
+        #install.packages("dplyr")
+        library(dplyr)
+        #install.packages("dtplyr")
+        library(dtplyr)
+        #install.packages("ggplot2")
+        library(ggplot2)
